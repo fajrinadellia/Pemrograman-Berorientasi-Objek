ac11c864898b88a07615ba1d4d1c241f62da866e
fajrinadellia <89902081+fajrinadellia@users.noreply.github.com>
2022-08-30 16:12:49 +0700

    Add files via upload

---
+++ MyProject2/src/myproject2/MyMainClass.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myproject2;
+
+/**
+ *
+ * @author user
+ */
+public class MyMainClass {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("FFFFFF          AA            JJJJJJ");
+        System.out.println("FF            AA  AA              JJ");
+        System.out.println("FF           AA    AA             JJ");
+        System.out.println("FFFFFF      AAAAAAAAAA            JJ");
+        System.out.println("FF         AA        AA           JJ");
+        System.out.println("FF        AA          AA     JJ   JJ");
+        System.out.println("FF       AA            AA     JJJJJ");
+    }
+    
+}
