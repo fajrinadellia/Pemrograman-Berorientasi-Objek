c168aaffb37a20de08ea9c654e66ae4d60a82629
fajrinadellia <89902081+fajrinadellia@users.noreply.github.com>
2023-01-02 12:09:52 +0700

    Add files via upload

---
+++ WrapperClass/src/wrapperclass/WrapperClass.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wrapperclass;
+import java.util.Scanner;
+/**
+ *
+ * @author user
+ */
+public class WrapperClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Integer a = 30;
+       System.out.println("Munculnya adalah "+ a.toString());
+    }
+
+}
+   
